64f1f80834cc62e7218db29139aed7759318b80a
Jose Rojas <jos.rojasg@duocuc.cl>
2026-05-17 16:58:33 -0400

    Fix a Devoluciones

--- Microservicios/Devolucion/Devolucion/src/main/java/cl/duoc/Devolucion/Repository/DevolucionRepository.java
+++ Microservicios/Devolucion/Devolucion/src/main/java/cl/duoc/Devolucion/Repository/DevolucionRepository.java
@@ -1,8 +1,5 @@
 package cl.duoc.Devolucion.repository;
 
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +7,5 @@ import cl.duoc.Devolucion.model.Devolucion;
 
 @Repository
 public interface DevolucionRepository extends JpaRepository<Devolucion, Integer> {
-    List<Devolucion> findByFecha(Date fecha_devolucion);
+    
 }
