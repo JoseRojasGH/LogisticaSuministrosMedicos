d2866bf1f0e5cdc18e6b6130df8324af0df984eb
Jose Rojas <jos.rojasg@duocuc.cl>
2026-06-06 13:55:55 -0400

    Eureka + POMS + Documentacion Cliente

--- Microservicios/Cliente/src/main/java/cl/duoc/Cliente/controller/ClienteController.java
+++ Microservicios/Cliente/src/main/java/cl/duoc/Cliente/controller/ClienteController.java
@@ -15,16 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 import cl.duoc.Cliente.dto.ClienteDTO;
 import cl.duoc.Cliente.model.Cliente;
 import cl.duoc.Cliente.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @RestController
 @RequestMapping("/api/v1/clientes")
+@Tag(name = "Clientes", description = "Operacion sobre los clientes del sistema")
 public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
 
     @GetMapping
+    @Operation(summary = "Busca todos los Clientes")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Clientes Encontrados"),
+                            @ApiResponse(responseCode = "204", description = "No se encontraron Clientes"),
+                            @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+    })
     public ResponseEntity<List<Cliente>> listarClientes(){
         List<Cliente> clientes = clienteService.listarClientes();
 
@@ -35,6 +45,12 @@ public class ClienteController {
     }
 
     @GetMapping("/id/{id}")
+    @Operation(summary = "Busca un Cliente por ID", 
+                description = "Retorna un Cliente según el ID proporcionado")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Cliente Encontrado"),
+                            @ApiResponse(responseCode = "404", description = "Cliente no Encontrado"),
+                            @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+    })
     public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id){
         try {
             Cliente cliente = clienteService.buscarClientePorId(id);
@@ -45,6 +61,12 @@ public class ClienteController {
     }
 
     @GetMapping("/rut/{rut}")
+    @Operation(summary = "Busca un Cliente por RUT", 
+                description = "Retorna un Cliente según el RUT proporcionado")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Cliente Encontrado"),
+                            @ApiResponse(responseCode = "404", description = "Cliente no Encontrado"),
+                            @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+    })
     public ResponseEntity<Cliente> getClienteByRut(@PathVariable String rut){
         try {
             Cliente cliente = clienteService.buscarClientePorRut(rut);
@@ -55,11 +77,21 @@ public class ClienteController {
     }
 
     @PostMapping
+    @Operation(summary = "Crea un nuevo Cliente")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Cliente Creado"),
+                            @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+    })
     public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente){
         return ResponseEntity.ok(clienteService.crearCliente(cliente));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Modifica un Cliente", 
+                description = "Modifica un Cliente según el ID proporcionado")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Cliente se ha Modificado"),
+                            @ApiResponse(responseCode = "404", description = "Cliente no Encontrado"),
+                            @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+    })
     public ResponseEntity<Cliente> updateCliente(@PathVariable Integer id, @RequestBody Cliente cliente){
         try {
             Cliente clienteActualizado = clienteService.actualizarCliente(id, cliente);
@@ -70,6 +102,11 @@ public class ClienteController {
     }
 
     @GetMapping("/dto/{id}")
+    @Operation(summary = "Busca la información del DTO de un cliente", 
+                description = "Retorna la información importante de un Cliente según el ID proporcionado")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Cliente Encontrado"),
+                            @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+    })
     public ResponseEntity<ClienteDTO> obtenerClienteDTO(@PathVariable Integer id) {
         Cliente cliente = clienteService.buscarClientePorId(id);
 
--- Microservicios/Producto/src/main/java/cl/duoc/Producto/client/UsuarioClient.java
+++ Microservicios/Producto/src/main/java/cl/duoc/Producto/client/UsuarioClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import cl.duoc.Producto.dto.UsuarioDTO;
 
-@FeignClient(name = "Usuario", url = "http://localhost:8081")
+@FeignClient(name = "Usuario")
 public interface UsuarioClient {
     @GetMapping("/api/v1/usuarios/dto/{id}")
     UsuarioDTO obtenerUsuario(@PathVariable("id") Integer id);
