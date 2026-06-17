c549e06116fd715b3de1acc33e61bf9c44e7c9d6
Galidson Munique <ga.munique@duocuc.cl>
2026-06-17 01:16:04 -0400

    feat: update usuario microservice controller

--- Microservicios/Usuario/Usuario/src/main/java/cl/duoc/Usuario/controller/UsuarioController.java
+++ Microservicios/Usuario/Usuario/src/main/java/cl/duoc/Usuario/controller/UsuarioController.java
@@ -16,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import cl.duoc.Usuario.dto.UsuarioDTO;
 import cl.duoc.Usuario.model.Usuario;
 import cl.duoc.Usuario.service.UsuarioService;
-import cl.duoc.Usuario.dto.UsuarioDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController 
 @RequestMapping("api/v1/usuarios")
