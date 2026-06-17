524fa817543f493befaaf57f3fedaa1622a2d182
Galidson Munique <ga.munique@duocuc.cl>
2026-06-17 00:49:59 -0400

    feat: add swagger config and update usuario microservice models and controller

--- Microservicios/Usuario/Usuario/src/main/java/cl/duoc/Usuario/controller/UsuarioController.java
+++ Microservicios/Usuario/Usuario/src/main/java/cl/duoc/Usuario/controller/UsuarioController.java
@@ -4,20 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import cl.duoc.Usuario.dto.UsuarioDTO;
 import cl.duoc.Usuario.model.Usuario;
 import cl.duoc.Usuario.service.UsuarioService;
-import cl.duoc.Usuario.dto.UsuarioDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController 
 @RequestMapping("api/v1/usuarios")
+@Tag(name = "Usuario", description = "Operacion sobre los usuarios del sistema")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping
+    @Operation(summary = "Lista todos los usuarios en el sistema")
     public ResponseEntity<List<Usuario>> listarUsuarios(){
         List<Usuario> usuarios = usuarioService.listarUsuarios();
         if(usuarios.isEmpty()){
@@ -27,6 +40,13 @@ public class UsuarioController {
     }
 
     @GetMapping("/id/{id}")
+    @Operation(summary = "Busca un usuario por ID",
+                description = "Retorna un usuario segun el ID Proporcionado")
+    @ApiResponses(value=  {@ApiResponse(responseCode = "200", description = "Usuario encontrado"),
+                          @ApiResponse(responseCode = "404",description = "Usuario no encontrado"),
+                          @ApiResponse(responseCode = "500",description = "Error interno del servidor")
+                          } 
+                )
     public ResponseEntity<Usuario> buscarporId(@PathVariable Integer id){
         try {
             Usuario usuario = usuarioService.buscarporId(id);
@@ -37,6 +57,13 @@ public class UsuarioController {
     }
 
     @GetMapping("/correo/{correo}")
+    @Operation(summary = "Busca un usuario por Correo",
+                description = "Retorna un usuario segun el Correo Proporcionado")
+    @ApiResponses(value= {@ApiResponse(responseCode = "200", description = "Usuario encontrado"),
+                          @ApiResponse(responseCode = "404",description = "Usuario no encontrado"),
+                          @ApiResponse(responseCode = "500",description = "Error interno del servidor")
+                          } 
+                )
     public ResponseEntity<Usuario> buscarporCorreo(@PathVariable String correo){
         try {
             Usuario usuario = usuarioService.buscarporCorreo(correo);
@@ -47,11 +74,21 @@ public class UsuarioController {
     }
 
     @PostMapping
+    @Operation(summary = "Crea un nuevo Usuario")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Usuario Creado"),
+                           @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+                           }   
+                 )
     public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario){
         return ResponseEntity.ok(usuarioService.crearUsuario(usuario));
     }
 
     @PatchMapping("/{id}/{contraseña}")
+    @Operation(summary = "Actualizar la contrasena del Usuario")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Usuario Actualizado"),
+                           @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+                           }
+                 )
     public ResponseEntity<Void> actualizarContraseñaUsuario(@PathVariable Integer id, @PathVariable String contraseña){
         try {
             usuarioService.actualizarContraseñaUsuario(id, contraseña);
@@ -62,6 +99,11 @@ public class UsuarioController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Elimina un Usuario")
+     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Usuario Actualizado"),
+                            @ApiResponse(responseCode = "500", description = "Error Interno del Servidor")
+                            }
+                  )
     public ResponseEntity<Void> eliminarUsuario(@PathVariable Integer id){
         try {
             usuarioService.eliminarUsuario(id);
