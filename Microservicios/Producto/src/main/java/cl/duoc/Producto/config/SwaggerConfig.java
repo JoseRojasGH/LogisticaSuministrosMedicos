cd18dd7101c505ad555219177286b3fd46715d55
Galidson Munique <ga.munique@duocuc.cl>
2026-06-21 00:14:38 -0400

    Feat: Agregar documentacion del Swagger Producto Categoria ProductoController

---
+++ Microservicios/Producto/src/main/java/cl/duoc/Producto/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package cl.duoc.Producto.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+        .info(new Info()
+                .title("Microservicio de Producto")
+                .version("1.0")
+                .description("Documentación de API Producto")
+            );
+    }
+
+}
--- Microservicios/Producto/src/main/java/cl/duoc/Producto/model/Categoria.java
+++ Microservicios/Producto/src/main/java/cl/duoc/Producto/model/Categoria.java
@@ -1,18 +1,30 @@
 package cl.duoc.Producto.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "categoria")
+@Schema(description = "Entidad que representa una categoría de producto en el sistema")
 public class Categoria {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Identificador único de la categoría", example = "1")
     private Integer id;
 
     @Column(nullable = false)
+    @Schema(description = "Nombre de la categoría", example = "Medicamentos")
     private String tipo_producto;
 }
